f5d2605870dbde72de5ec5b16111c6f64b320cdb
大诚专用 <83280028+ChenCheng19981014@users.noreply.github.com>
2022-10-23 20:50:55 +0800

    自适应布局

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,112 +1,98 @@
 <template>
   <div class="farther">
-    <div class="bar">
-      <div class="animation" ref="animaDom"></div>
-      <div
-        v-for="(i, index) in arr"
-        :class="index === cssIndex ? `${i} selectColor` : i"
-        @click="changeCss(i, index)"
-      >
-        {{ i }}
-      </div>
-    </div>
+    <div class="box"></div>
   </div>
 </template>
 
-<script lang="ts" setup>
-//@ts-ignore
-import MySon from "./../components/son.vue";
-import { myMixins } from "../../mixins";
-import { getJson } from "../../api";
-import { fn } from "./../../mixin/mixin";
-import { ref } from "vue";
+<script setup lang="ts">
+import { TweenMax } from "gsap";
+import { ref, onMounted } from "vue";
 
-const arr = ["a", "b", "c"];
-let cssIndex = ref(0);
-let animaDom = ref(null);
-let lastIndex = 0;
-// :class="index === cssIndex ? `${i} bgc` : i"
+interface Function1 {
+  func(str: string): void;
+}
 
-const changeCss = (i: string, index: any) => {
-  if (lastIndex === index) return;
+type Function2 = (a: string, b: string) => {};
 
-  cssIndex.value = index;
-
-  (animaDom as any).value.style["left"] = `${index * 100 + index * 2 + 1}px `;
-
-  lastIndex = index;
-
-  console.log(index * 100 + index + 1, "left的值");
+const fn1: Function1["func"] = (str) => {
+  return str;
 };
 
-/**
- *  块格式上下文的条件
- *  display inline-block  table-ceil
- *  position 不为 relative 而为absolute static
- *  float 不为:none
- *  overflow 不为visible  只为auto scroll hidden
- *  dynamic island
- */
+enum Function3 {
+  name,
+  age,
+  address,
+}
+
+let arr: Array<string> = [];
+let arr2: string[] = [];
+
+const fn3 = <T>(param: T): T => {
+  return param;
+};
+
+let num3 = fn3<number>(123);
+
+console.log(num3, "num3");
+
+onMounted(() => {
+  function init() {
+    const winWidth = document.documentElement.clientWidth;
+    const winHeight = document.documentElement.clientHeight;
+    // 屏幕比
+    const winScale = winWidth / winHeight;
+
+    let page = document.querySelector(".farther");
+    const pageWidth = 750;
+    const pageHeight = 1334;
+    // 视口比
+    const pageScale = pageWidth / pageHeight;
+
+    const origin = "50% 50% 0";
+
+    let initialScale = 1;
+
+    if (winScale > pageScale) {
+      // 宽度长了，但是高度不够
+      // 高度占满，宽度水平居中
+      console.log("高度占满，宽度水平居中");
+      initialScale = winHeight / pageHeight;
+    } else {
+      console.log("宽度占满，高度垂直居中");
+      // 高度长了，但是宽度不够
+      // 宽度占满，高度垂直居中
+      initialScale = winWidth / pageWidth;
+    }
+
+    (page as any).style.width = pageWidth + "px";
+    (page as any).style.height = pageHeight + "px";
+    (page as any).style.transform = "scale(" + initialScale + ")";
+    (page as any).style.transformOrigin = origin;
+    (page as any).style.left = (pageWidth - winWidth) / -2 + "px";
+    (page as any).style.top = (pageHeight - winHeight) / -2 + "px";
+  }
+  init();
+  window.addEventListener("resize", init);
+});
 </script>
 
-<style lang="less">
+<style scoped>
 .farther {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #555;
-}
-.farther .bar {
-  width: auto;
-  display: flex;
-  justify-content: center;
-  position: relative;
-}
-.bar > div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100px;
-  height: 50px;
-  transition: all 1s;
-}
-.bgc {
-  background-color: skyblue;
-  opacity: 1 !important;
-}
-
-.animation {
-  .bgc;
-  color: black;
-  border-radius: 10px;
+  width: 750px;
+  height: 1334px;
   position: absolute;
-  left: 1px;
+  left: 0;
+  top: 0;
+  border: 1px solid red;
+  position: relative;
+  background-color: #666;
 }
-
-.selectColor {
-  color: black;
-  opacity: 1 !important;
-  z-index: 2;
-}
-
-.bar > div:hover {
-  opacity: 0.1;
-  background-color: skyblue;
-  transition: all 1s;
-}
-
-.a {
-  border: 1px solid #888;
-  border-radius: 10px;
-}
-.b {
-  border: 1px solid #888;
-  border-radius: 10px;
-}
-.c {
-  border: 1px solid #888;
-  border-radius: 10px;
+.farther .box {
+  width: 91px;
+  height: 92px;
+  position: absolute;
+  left: 500px;
+  top: 15px;
+  background-color: pink;
 }
 </style>
